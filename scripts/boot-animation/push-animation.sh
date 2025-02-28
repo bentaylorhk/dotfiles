e79ebdec6864988616dacde6ebdf4b9e501fda1e
Benjamin Taylor <bentaylorhk@gmail.com>
2025-02-28 00:28:07 -0700

    Polished i3 and X envs

--- scripts/boot-animation/push-animation.sh
+++ scripts/boot-animation/push-animation.sh
@@ -14,6 +14,7 @@ sudo cp ~/ascii-art/polyOS/caligraphy.txt /usr/local/share/ascii-art/boot.txt
 sudo chmod +x $ANIMATION_DIR/boot-animation.sh
 sudo cp $ANIMATION_DIR/boot-animation.sh /usr/local/bin/.
 sudo cp $ANIMATION_DIR/splash /usr/local/bin/.
-sudo cp ~/systemd/boot-animation.service /etc/systemd/system/.
+sudo cp -r ~/systemd /etc/systemd/system/.
 
 sudo systemctl enable boot-animation.service
+sudo systemctl enable getty@tty1.service
