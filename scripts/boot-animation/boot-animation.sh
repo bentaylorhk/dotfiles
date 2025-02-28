4af420f8f46bf8fee7aad292e239127eba10b0ab
Benjamin Taylor <benjamin.taylor@arm.com>
2025-02-27 22:05:55 -0700

    Added boot animation mechanisms

---
+++ scripts/boot-animation/boot-animation.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Benjamin Michael Taylor (bentaylorhk)
+# 2025
+set -e
+
+export TERM=linux
+
+clear
+/usr/local/bin/splash /usr/local/share/ascii-art/boot.txt
+
+# Doubling font size for login readability
+setfont -d
---
+++ scripts/boot-animation/push-animation.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Benjamin Michael Taylor (bentaylorhk)
+# 2025
+
+# Script which pushes animation files maintined upstream to
+# relavant system files, and enables the service.
+
+set -e
+
+ANIMATION_DIR=~/scripts/boot-animation
+
+sudo cp ~/ascii-art/polyOS/caligraphy.txt /usr/local/share/ascii-art/boot.txt
+
+sudo chmod +x $ANIMATION_DIR/boot-animation.sh
+sudo cp $ANIMATION_DIR/boot-animation.sh /usr/local/bin/.
+sudo cp $ANIMATION_DIR/splash /usr/local/bin/.
+sudo cp ~/systemd/boot-animation.service /etc/systemd/system/.
+
+sudo systemctl enable boot-animation.service
