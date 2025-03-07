0bff39c02adc2996383606ae076e12bcbe8a63a4
Benjamin Taylor <bentaylorhk@gmail.com>
2025-03-07 15:05:20 -0700

    Renable nvim smear

--- .config/nvim/lua/plugins/smear-cursor.lua
+++ .config/nvim/lua/plugins/smear-cursor.lua
@@ -3,4 +3,14 @@
 
 return {
     "sphamba/smear-cursor.nvim",
+    opts = {
+        {
+            -- Match grubbox light fg
+            cursor_color = "#654735",
+            --stiffness = 0.6,
+            --trailing_stiffness = 0.1,
+            trailing_exponent = 1,
+            --gamma = 1,
+        },
+    },
 }
