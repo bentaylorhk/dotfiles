113bd65921794d08045f1a115546a9c5f3d3b4f4
Benjamin Taylor <bentaylorhk@gmail.com>
2025-03-12 07:07:25 -0600

    Create core nvim plugin

--- scripts/update-grub.sh
+++ scripts/update-grub.sh
@@ -1,7 +1,9 @@
 #! /bin/sh
 # Benjamin Michael Taylor (bentaylorhk)
-# 2023 - 2024
+# 2023 - 2025
 set -e
+
+# TODO: set Divide script up by hostname
 
 mount /dev/nvme0n1p1 /windows
 grub-mkconfig -o /boot/grub/grub.cfg
