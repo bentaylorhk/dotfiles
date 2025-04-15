532fe2bf871e74f4af13ec01ad427ac246e96dc4
Benjamin Taylor <bentaylorhk@gmail.com>
2025-04-15 15:57:49 +0100

    Fix boot animation cross system

--- scripts/startup/startup.sh
+++ scripts/startup/startup.sh
@@ -8,7 +8,7 @@ setfont -d
 
 # Boot splash
 clear
-/usr/local/bin/splash /usr/local/share/ascii-art/boot.txt
+/usr/local/bin/boot-animation /usr/local/share/ascii-art/boot.txt
 
 # Injecting login banner
 /usr/local/bin/right-justify.sh /usr/local/share/ascii-art/login.txt | sed 's/\\/\\\\/g' >/etc/issue
