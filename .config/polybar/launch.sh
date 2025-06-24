3e3216e6cc27467534e5966487a5e075be9e62a7
Benjamin Taylor <bentaylorhk@gmail.com>
2025-06-24 13:31:24 +0100

    Dynamic polybar loading per monitor

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+# Benjamin Michael Taylor (bentaylorhk)
+# 2025
+
+killall -q polybar
+
+while pgrep -u $UID -x polybar >/dev/null; do sleep 0.1; done
+
+for m in $(polybar --list-monitors | cut -d: -f1); do
+  if [ "$m" = "DVI-0" ]; then
+      MONITOR=$m polybar crt &
+  else
+      MONITOR=$m polybar main &
+  fi
+done
