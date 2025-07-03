bd908c59e463ab569d53cde49f25b6877fef95a1
Benjamin Taylor <bentaylorhk@gmail.com>
2025-07-03 03:15:46 +0100

    More working grub theme

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,7 +5,7 @@
 
 killall -q polybar
 
-while pgrep -u $UID -x polybar >/dev/null; do sleep 0.1; done
+while pgrep -u $UID -x polybar >/dev/null; do sleep 0.01; done
 
 for m in $(polybar --list-monitors | cut -d: -f1); do
   if [ "$m" = "DVI-0" ]; then
